37d66972659cfdcd0357cb7ff46e8786d9863679
Fekete Márk <fmark98@gmail.com>
2019-10-09 17:02:01 +0200

    Finished without any detected bug.

--- Pickpoket/Pickpoket/Pickpoket.cpp
+++ Pickpoket/Pickpoket/Pickpoket.cpp
@@ -1,20 +1,51 @@
-// Pickpoket.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+struct thief
+{
+	int height;
+	string eyeColor;
+	string hairColor;
+	int downloadedPunishment;
+};
+
+int machCount(thief t1, thief t2)
+{
+	int result = 0;
+	if (t1.height == t2.height) { ++result; }
+	if (t1.eyeColor == t2.eyeColor) { ++result; }
+	if (t1.hairColor == t2.hairColor) { ++result; }
+	if (t1.downloadedPunishment == t2.downloadedPunishment) { ++result; }
+	return result;
+}
+
+thief thiefs[100];
+const int MAX_N = 100;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> thiefs[i].height >> thiefs[i].eyeColor >> thiefs[i].hairColor >> thiefs[i].downloadedPunishment;
+	}
+	int i = 0;
+	bool found = false;
+	while (i < n && !found)
+	{
+		int j = i + 1;
+		while (j < n && !found)
+		{
+			if (machCount(thiefs[i], thiefs[j]) > 1)
+			{
+				found = true;
+			}
+			++j;
+		}
+		++i;
+	}
+	cout <<(found ? "IGEN" : "NEM");
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
